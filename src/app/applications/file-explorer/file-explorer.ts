47a27ab72254c115293cb27113a395342736efc9
Phillip <phillipstephan314@gmail.com>
2025-09-25 18:52:16 +0200

    basic file explorer

--- src/app/applications/file-explorer/file-explorer.ts
+++ src/app/applications/file-explorer/file-explorer.ts
@@ -1,17 +1,22 @@
 import {Component, Type} from '@angular/core';
 import {Application, WindowComponent} from "../window/window";
+import {FileSystemManager} from "../../filesystem/filesystem_manager";
+import {NgForOf, NgStyle} from "@angular/common";
+import {Folder} from "../../filesystem/folder_object";
 
 @Component({
   selector: 'app-file-explorer',
   imports: [
-    WindowComponent
+    WindowComponent,
+    NgForOf,
+    NgStyle
   ],
   templateUrl: './file-explorer.html',
   styleUrl: './file-explorer.scss'
 })
 export class FileExplorer implements Application{
   name = "FileExplorer"
-
+  selectedFolder: Folder = FileSystemManager.desktop;
   getComponent(): typeof FileExplorer {
     return FileExplorer;
   }
@@ -19,4 +24,6 @@ export class FileExplorer implements Application{
   getFileExplorer() {
     return this;
   }
+
+  protected readonly FileSystemManager = FileSystemManager;
 }
